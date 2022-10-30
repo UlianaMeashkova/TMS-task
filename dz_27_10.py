b2d92ab9bd471187805e932990127587b1266fc8
UlianaMeashkova <el.mahno@gmail.com>
2022-10-30 18:21:50 +0300

    First, Second, Third Task

---
+++ dz_27_10.py
@@ -0,0 +1,71 @@
+
+# a = float(input("Введите первое число: "))
+# b = float(input("Введите второе число: "))
+# c = float(input("Введите третье число: "))
+# print(max(a, b, c))
+
+# D = input("Что делаем? +, -, *, /: ")
+# a = float(input("Введите первое число: "))
+# b = float(input("Введите второе число: "))
+# if D == "+":
+#     c = a + b
+#     print("Результат: " + str(c))
+# elif D == "-":
+#     c = a - b
+#     print("Результат: " + str(c))
+# elif D == "*":
+#     c == a * b
+#     print("Результат: " + str(c))
+# elif D == "/":
+#     c == a / b
+#     print("Результат: " + str(c))
+# else:
+#     print("Операция выбрана неверно")
+
+# # first task
+a = {}
+b = a
+c = b
+
+print(id(a))
+print(id(b))
+print(id(c))
+
+print(a)
+print(b)
+print(c)
+
+# # second task
+d = {'a': 1}
+e = {'a': 1}
+
+print(id(d))
+print(id(e))
+
+print(d)
+print(e)
+
+# 3 task
+d = e
+print(id(d))
+print(id(e))
+
+a = {}
+b = {}
+c = {}
+print(id(a))
+print(id(b))
+print(id(c))
+
+# 4 task
+
+D = int(input("Введите произвольную строку: "))
+
+
+
+
+
+
+
+
+
