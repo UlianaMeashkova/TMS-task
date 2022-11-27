d557196033f9dcc377c0d173a4216f9314b1e568
UlianaMeashkova <el.mahno@gmail.com>
2022-11-27 20:29:28 +0300

    dz 2 tasks

---
+++ dz24_11.py
@@ -0,0 +1,20 @@
+#1
+card = input('Введите номер карты: ')
+def card_hide(card123):
+    return '*' * (len(card123)-4) + card123[-4:]
+a = card_hide(card)
+print(a)
+
+# #2
+# def isPalindrome(string):
+# 	left_pos = 0
+# 	right_pos = len(string) - 1
+	
+# 	while right_pos >= left_pos:
+# 		if not string[left_pos] == string[right_pos]:
+# 			return False
+# 		left_pos += 1
+# 		right_pos -= 1
+# 	return True
+# print(isPalindrome('rttttr')) 
+
